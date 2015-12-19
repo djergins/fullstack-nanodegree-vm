481e66bcf353bc7020c45478550f6a72e39fb1ca
djergins <danieljergins@gmail.com>
2015-12-19 10:16:33 -0600

    Add ability to delete old matches.

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -8,11 +8,15 @@ import psycopg2
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=tournament")
+    return psycopg2.connect("dbname=tournament") 
 
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    pg = connect()
+    c = pg.cursor()
+    c.execute("delete from matches")
+
 
 
 def deletePlayers():

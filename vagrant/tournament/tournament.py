292a17392c3d589e261ade32342b437db1f48dc0
djergins <danieljergins@gmail.com>
2016-01-09 17:12:39 -0600

    update to be pep8 compliant.

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -8,7 +8,7 @@ import psycopg2
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=tournament") 
+    return psycopg2.connect("dbname=tournament")
 
 
 def deleteMatches():
@@ -38,12 +38,13 @@ def countPlayers():
     pg.close()
     return result
 
+
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
@@ -54,9 +55,10 @@ def registerPlayer(name):
     pg.commit()
     pg.close()
 
+
 def countMatches():
-    """Returns a list of matches. This is used to confirm 
-    that our code is preventing rematches in the 
+    """Returns a list of matches. This is used to confirm
+    that our code is preventing rematches in the
     testPreventRematches function.
     """
     pg = connect()
@@ -66,10 +68,12 @@ def countMatches():
     pg.close()
     return result
 
+
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
+    The first entry in the list should
+    be the player in first place, or a player
     tied for first place if there is currently a tie.
 
     Returns:
@@ -109,22 +113,23 @@ def reportMatch(winner, loser):
     pg = connect()
     c = pg.cursor()
     query = "insert into matches (winner, loser) values (%s, %s)"
-    try: 
+    try:
         c.execute(query, (winner, loser,))
         pg.commit()
 
     except psycopg2.Error as e:
         pass
-    pg.close() 
- 
+    pg.close()
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -132,7 +137,7 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-    
+
     pg = connect()
     c = pg.cursor()
     c.execute("select * from pairings")
@@ -140,5 +145,3 @@ def swissPairings():
     pg.commit()
     pg.close()
     return result
-
-
--- vagrant/tournament/tournament_test.py
+++ vagrant/tournament/tournament_test.py
@@ -4,6 +4,7 @@
 
 from tournament import *
 
+
 def testDeleteMatches():
     deleteMatches()
     print "1. Old matches can be deleted."
@@ -63,7 +64,8 @@ def testStandingsBeforeMatches():
     registerPlayer("Randy Schwartz")
     standings = playerStandings()
     if len(standings) < 2:
-        raise ValueError("Players should appear in playerStandings even before "
+        raise ValueError("Players should appear in "
+                         "playerStandings even before "
                          "they have played any matches.")
     elif len(standings) > 2:
         raise ValueError("Only registered players should appear in standings.")
@@ -74,9 +76,11 @@ def testStandingsBeforeMatches():
         raise ValueError(
             "Newly registered players should have no matches or wins.")
     if set([name1, name2]) != set(["Melpomene Murray", "Randy Schwartz"]):
-        raise ValueError("Registered players' names should appear in standings, "
+        raise ValueError("Registered players' names "
+                         "should appear in standings, "
                          "even if they have no matches played.")
-    print "6. Newly registered players appear in the standings with no matches."
+    print "6. Newly registered players "
+    "appear in the standings with no matches."
 
 
 def testReportMatches():
@@ -97,7 +101,8 @@ def testReportMatches():
         if i in (id1, id3) and w != 1:
             raise ValueError("Each match winner should have one win recorded.")
         elif i in (id2, id4) and w != 0:
-            raise ValueError("Each match loser should have zero wins recorded.")
+            raise ValueError("Each match loser should "
+                             "have zero wins recorded.")
     print "7. After a match, players have updated standings."
 
 
@@ -124,6 +129,7 @@ def testPairings():
             "After one match, players with one win should be paired.")
     print "8. After one match, players with one win are paired."
 
+
 def testPreventRematches():
     deleteMatches()
     deletePlayers()
@@ -135,8 +141,8 @@ def testPreventRematches():
     [id1, id2, id3, id4] = [row[0] for row in standings]
     reportMatch(id1, id2)
     reportMatch(id3, id4)
-    # try different combinations to make sure the 
-    # the rematch is prevented regardless of order. 
+    # try different combinations to make sure the
+    # the rematch is prevented regardless of order.
     reportMatch(id2, id1)
     reportMatch(id4, id3)
     c = countMatches()
@@ -144,6 +150,7 @@ def testPreventRematches():
         raise ValueError(
             "Players should not be able to face each other again.")
     print "9. Players face each other only once in a tournament."
+
 
 def testRoundBye():
     deleteMatches()
@@ -155,14 +162,14 @@ def testRoundBye():
     registerPlayer("Juggernaut")
     registerPlayer("Rogue")
     # Standings will round out odd numbers of players
-    # to even by registering a 'bye' player. 
+    # to even by registering a 'bye' player.
     standings = playerStandings()
     [id1, id2, id3, id4, id5, id6] = [row[0] for row in standings]
     reportMatch(id1, id2)
     reportMatch(id3, id4)
     reportMatch(id5, id6)
-    #due to the unique constraint preventing rematches
-    #players cannot rematch the 'bye'.
+    # due to the unique constraint preventing rematches
+    # players cannot rematch the 'bye'.
     reportMatch(id5, id6)
     c = countMatches()
     if c != 3:
@@ -182,5 +189,3 @@ if __name__ == '__main__':
     testPreventRematches()
     testRoundBye()
     print "Success!  All tests pass!"
-
-
